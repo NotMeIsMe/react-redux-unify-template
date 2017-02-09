79520bc2c04b3de21465ed68f118abfd86110ac4
haskell <2637309949@qq.com>
2017-02-10 00:02:14 +0800

    暂存

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -34,7 +34,11 @@ class Tab extends React.Component {
               </div>
               <div className={styles.tabRightWrap}>
                 {
-                  rightComs.flatMap((value, k) => value.map((value2, k2) => <div className={k2 === 0 && styles.spec} id={k2} key={(k2 + 1) * (k + 100)}>{k2 === 0 && <div className={styles.classTitle}>{leftComs[k]}</div>}{value2}</div>))
+                  rightComs.flatMap((value, k) =>
+                    value.map((value2, k2) => ({ isclass: k2 === 0, classIndex: k, content: value2 }))
+                  ).map((value3, k3) =>
+                    <div className={value3.isclass === true && styles.spec} id={k3} key={k3 + 100}>{value3.isclass && <div className={styles.classTitle}>{leftComs[value3.classIndex]}</div>}</div>
+                  )
                 }
               </div>
           </div>;
