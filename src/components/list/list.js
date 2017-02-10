f5f11b3c0e8732fb2d7b0f31dade1a5a9653f630
haskell <2637309949@qq.com>
2017-02-10 18:05:14 +0800

    添加list控件

---
+++ src/components/list/list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './list.css';
+class List extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      lData: [
+            {title: 'x'}, {title: 's'}, {title: 'r'}
+      ]};
+  }
+  render () {
+    return <div className={styles.list}>
+                {
+                    this.state.lData.map((s, k) => {
+                      return <div key={k}>{s.title}
+                        <span />
+                      </div>;
+                    })
+                }
+           </div>;
+  }
+}
+
+export default List;
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Hello from '../../components/hello/hello';
+// import Hello from '../../components/hello/hello';
 import NavBar from '../../components/navbar/navbar';
 import Tab from '../../components/tab/tab';
 import FloatBt from '../../components/floatbt/button';
+import List from '../../components/list/list';
 import styles from './home.css';
 
 class Home extends React.Component {
   render () {
     return <div className={styles.home}>
-                <NavBar contents={[<div><Hello /> <FloatBt /></div>,
+                <NavBar contents={[<div><List /> <FloatBt /></div>,
                   <Tab rData={[{ className: '测试1', arrs: [ <div />, <div /> ] },
                   { className: '测试2', arrs: [ <div />, <div />, <div /> ] },
                   { className: '测试3', arrs: [ <div />, <div />, <div />, <div />, <div />, <div />, <div />, <div />, <div /> ] }]}/>
